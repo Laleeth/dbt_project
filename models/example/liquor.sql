f33990f6933c0ba130d91547a4b1167f4618ce11
Lalith Sai Thomala <thomalalalithsai@gmail.com>
2023-07-07 12:53:35 +0000

    liqour_sales

--- models/example/liquor.sql
+++ models/example/liquor.sql
@@ -1,3 +1,4 @@
 select item_number,sum(bottles_sold) as Total_bottles_sold,count(bottles_sold) as no_of_bottles from cogent-chess-289709.dbt_lthomala.liquor_sales
 group by item_number
 order by Total_bottles_sold desc
+limit 50;
